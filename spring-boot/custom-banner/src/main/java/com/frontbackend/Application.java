8eef9759c10bc0f1a134631b3716f1dc2f63598a
Marcin Wojtysiak <marcin.wojtysiak@espeo.eu>
2019-06-15 23:41:19 +0200

    add custom-banner project

---
+++ spring-boot/custom-banner/src/main/java/com/frontbackend/Application.java
@@ -0,0 +1,14 @@
+package com.frontbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
