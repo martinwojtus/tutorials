a2e7cab13677d3b7717d8ccaee301119a3cd42b9
Marcin Wojtysiak <marcin.wojtysiak@espeo.eu>
2020-11-02 22:19:00 +0100

    Thymeleaf input hidden

---
+++ thymeleaf/thymeleaf-hidden-input/src/main/java/com/frontbackend/thymeleaf/bootstrap/model/Form.java
@@ -0,0 +1,13 @@
+package com.frontbackend.thymeleaf.bootstrap.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class Form {
+
+    private String email;
+}
